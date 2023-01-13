a0adefbb48eb41d503d5f22bb17baa77e85aed33
adammkeane <adamkeane.edu@gmail.com>
2023-01-13 15:20:09 +0000

    Finish last of jest videos for the simon game walkthrough project

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -1,8 +1,17 @@
+/**
+ * @jest-environment jsdom
+ */
+
+
 const buttonClick = require("../button");
 const button = require("../button");
 
 beforeEach(() => {
-    document.body.innerHTML = "<p id='par'></p>";
+   let fs = require("fs");
+   let fileContents = fs.readFileSync("index.html", "utf-8");
+   document.open();
+   document.write(fileContents);
+   document.close();
 });
 
 describe("DOM tests", () => {
@@ -10,4 +19,7 @@ describe("DOM tests", () => {
         buttonClick();
         expect(document.getElementById("par").innerHTML).toEqual("You Clicked");
     });
+    test("h1 should exist", () => {
+        expect(document.getElementsByTagName("h1").length).toBe(1);
+    })
 });
